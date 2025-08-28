3b0cd40624eae61f5c4bcb3366b261a7534dec98
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-08-28 07:26:53 +0200

    Init rust project

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25-05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     naersk.url = "github:nix-community/naersk";
   };
 
   outputs = {
+    self,
     nixpkgs,
     naersk,
   }: let
@@ -15,6 +16,23 @@
     packages.${system}.default = naerskLib.buildPackage {
       src = ./.;
       cargoLock = ./Cargo.lock;
+      buildInputs = [pkgs.glib];
+      nativeBuildInputs = [pkgs.pkg-config];
+    };
+
+    shells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        cargo
+        rustc
+        rustfmt
+        clippy
+        rust-analyzer
+        glib
+      ];
+
+      nativeBuildInputs = [pkgs.pkg-config];
+
+      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
     };
   };
 }
